8864292871ce7cc95c1bcb61f3e47e1e6bed8aa1
Momo Ueda <umomo88@gmail.com>
2021-10-25 22:57:17 -0700

    download eventbrite csv and select 'yes's

--- eventbrite_to_mailchimp.py
+++ eventbrite_to_mailchimp.py
@@ -1,4 +1,4 @@
-#this code will get the attendee list from eventbrite, get the emails of those who said yes to receiving a newsletter, and upload into mailchimp
+#this code will get the attendee list for the newest event from eventbrite, get the emails of those who said yes to receiving a newsletter, and upload into mailchimp
 import requests
 import json
 from configparser import ConfigParser
@@ -15,12 +15,17 @@ event_id = config['eventbrite_ids']['event_id']#update event_id for each event
 response = requests.get("https://www.eventbriteapi.com/v3/users/me/?token=" + eventbrite_token) #log into eventbrite
 
 reports = requests.get( "https://www.eventbriteapi.com/v3/events/" +event_id+ "/attendees/?token=" + eventbrite_token) #get event details
-attendees = reports.json()['attendees']
+attendees = reports.json()['attendees'] #get attendee reports
 
+#create a dataframe with attendee email, first name, last name and questions
+df = pd.json_normalize(attendees, ['answers'], [['profile','email'], ['profile', 'first_name'], ['profile', 'last_name']], errors = 'ignore')
 
-df = pd.json_normalize(attendees)
-df1 = df['answers']
-df1
+#subset only those rows with the mailing list question
+df_mail = df[df['question'] == "Do you agree to receive future events and news from the event organizers?"]
 
+#subset only those rows that say yes
+df_yes = df_mail[df_mail["answer"].str.contains("Yes")]
+new_yes_list = df_yes[['profile.first_name', 'profile.last_name', 'profile.email']]
+new_yes_list
 
 #next check how we can get the dictionaries out to dataframe
